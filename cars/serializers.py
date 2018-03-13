3a79a57db4df0fead552b99c1f263bc7cbb1ee63
devtdk <devtdk@local.dev>
2018-03-13 10:43:39 +0100

    Some example validation

--- cars/serializers.py
+++ cars/serializers.py
@@ -16,6 +16,25 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class TypeSerializer(serializers.ModelSerializer):
+
+    def validate_name(self, value):
+        # example of field error exception (in specific validate method)
+        if value[0] == '5':
+            raise serializers.ValidationError("Nazwa nie może rozpoczynać się cyfrą 5")
+        return value
+
+    def validate(self, data):
+
+        # example of non-field-errors exception
+        if data['name'][0] == '1':
+            raise serializers.ValidationError("Nazwa nie może rozpoczynać się cyfrą 1")
+
+        # example of field error exception (in general validate)
+        if data['name'][0] == '2':
+            raise serializers.ValidationError({ 'name': "Nazwa nie może rozpoczynać się cyfrą 2"})
+
+        return data
+
     class Meta:
         model = models.Type
         fields = '__all__'
